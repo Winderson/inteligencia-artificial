724b4e40ae475098bf2ccf399452a4b6fce388ad
Winderson <winderson.wjbs@gmail.com>
2018-09-19 20:40:31 -0500

    busca em largura

---
+++ trabalho1/busca_custo_uniforme.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+'''
+
+
+
+
+'''
+
+class BuscaCustoUniforme(object):
+
+    pass
---
+++ trabalho1/problema_torre_hanoi.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+from problema import Problema
+
+class ProblemaTorreHanoi(Problema):
+    torreA = []
+    torreB = []
+    torreC = []
+
+    class Estado(object):
+        def __init__(self):
+            self.torreA = []
+            self.torreB = []
+            self.torreC = []
+            self.pai = None
+            self.custo = 0
+            self.acao = ''
+        def copy(self):
+            estado = ProblemaTorreHanoi.Estado()
+            estado.torreA = self.torreA.copy()
+            estado.torreB = self.torreB.copy()
+            estado.torreC = self.torreC.copy()
+            return estado
+        def __repr__(self):
+            return f'{self.torreA} \n {self.torreB} \n {self.torreC}'
+
+    @property
+    def estado_inicial(self):
+        estado = ProblemaTorreHanoi.Estado()
+        estado.torreA = [5, 4, 3, 2, 1]
+        estado.torreB = []
+        estado.torreC = []
+        estado.pai = None
+        return estado
+
+    def solucao(self):
+        solucao_final = []
+        while estado.pai is not None:
+            solucao_final.append(estado)
+            estado = estado.pai
+        solucao_final.append(estado)
+        return solucao_final.reverse()
+
+    def funcao_objetivo(self, estado):
+        return estado.torreC == [5, 4, 3, 2, 1]
+
+    def __mover_(self, estado_pai, acao):
+        estado = estado_pai.copy()
+        estado.acao = acao
+        tamA = len(estado.torreA)
+        tamB = len(estado.torreB)
+        tamC = len(estado.torreC)
+        if acao == 'A->B' and (estado.torreA[tamA - 1] < estado.torreB[tamB - 1] or tamB == 0):
+            estado.torreB.append(estado.torreA.pop(-1))
+        elif acao == 'A->C' and (estado.torreA[tamA - 1] < estado.torreC[tamC - 1] or tamC == 0):
+            estado.torreC.append(estado.torreA.pop(-1))
+        elif acao == 'B->A' and (estado.torreB[tamB - 1] < estado.torreA[tamA - 1] or tamA == 0):
+            estado.torreA.append(estado.torreB.pop(-1))
+        elif acao == 'B->C' and (estado.torreB[tamB - 1] < estado.torreC[tamC - 1] or tamC == 0):
+            estado.torreC.append(estado.torreB.pop(-1))
+        elif acao == 'C->A' and (estado.torreC[tamC - 1] < estado.torreA[tamA - 1] or tamA == 0):
+            estado.torreA.append(estado.torreC.pop(-1))
+        elif acao == 'C->B' and (estado.torreC[tamC - 1] < estado.torreB[tamB - 1] or tamB == 0):
+            estado.torreB.append(estado.torreC.pop(-1))
+        else:
+            return None
+        estado.pai = estado_pai
+        return estado
+
+    def __valida_restricoes(self, estado):
+        print(estado)
+        print(estado.torreA)
+        print(estado.torreB)
+        print(estado.torreC)
+
+    def funcao_sucessora(self, estado):
+        sucessores = []
+        a1 = self.__mover(estado, 'A->B')
+        a2 = self.__mover(estado, 'A->C')
+        a3 = self.__mover(estado, 'B->A')
+        a4 = self.__mover(estado, 'B->C')
+        a5 = self.__mover(estado, 'C->A')
+        a6 = self.__mover(estado, 'C->B')
+        if a1: sucessores.append(a1)
+        if a2: sucessores.append(a2)
+        if a3: sucessores.append(a3)
+        if a4: sucessores.append(a4)
+        if a5: sucessores.append(a5)
+        if a6: sucessores.append(a6)
+        return sucessores
